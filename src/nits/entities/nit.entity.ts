d376cce8049ddf1aaa64c2d5f46f50bea66eb223
JuanXhod <123758906+CODE-G-ROOT@users.noreply.github.com>
2025-01-24 21:38:13 -0500

    feat(Nits): :sparkles: entity, interface added

--- src/nits/entities/nit.entity.ts
+++ src/nits/entities/nit.entity.ts
@@ -1,1 +1,7 @@
-export class Nit {}
+export class Nit {
+  public id: number;
+  public id_user: number;
+  public status: string;
+  private created_at: Date;
+  private updated_at: Date;
+}
---
+++ src/nits/interface/nit.interface.ts
@@ -0,0 +1,11 @@
+export interface Nit {
+  id: number;
+  firstName: string;
+  secondName: string;
+  firstLastName: string;
+  secondLastName: string;
+  state: string;
+  description_state: string;
+  updated_at: Date;
+  created_at: Date;
+}
