fb6fbf6d4cd163ebb2bb590529f36767e4ab97f0
JuanXhod <123758906+CODE-G-ROOT@users.noreply.github.com>
2025-01-24 21:42:02 -0500

    feat(Nits): :sparkles: added CRUD
    CRUD for Nits routes has been added
    
    commit pending to validate errors

--- src/nits/nits.controller.ts
+++ src/nits/nits.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { NitsService } from './nits.service';
 import { CreateNitDto } from './dto/create-nit.dto';
 import { UpdateNitDto } from './dto/update-nit.dto';
@@ -7,9 +15,9 @@ import { UpdateNitDto } from './dto/update-nit.dto';
 export class NitsController {
   constructor(private readonly nitsService: NitsService) {}
 
-  @Post()
-  create(@Body() createNitDto: CreateNitDto) {
-    return this.nitsService.create(createNitDto);
+  @Post('/create')
+  create(@Body() body: CreateNitDto) {
+    return this.nitsService.create(body);
   }
 
   @Get()
@@ -22,12 +30,12 @@ export class NitsController {
     return this.nitsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('/update/:id')
   update(@Param('id') id: string, @Body() updateNitDto: UpdateNitDto) {
     return this.nitsService.update(+id, updateNitDto);
   }
 
-  @Delete(':id')
+  @Delete('/delete/:id')
   remove(@Param('id') id: string) {
     return this.nitsService.remove(+id);
   }
--- src/nits/nits.service.ts
+++ src/nits/nits.service.ts
@@ -1,26 +1,71 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNitDto } from './dto/create-nit.dto';
 import { UpdateNitDto } from './dto/update-nit.dto';
+import { Nit } from './interface/nit.interface';
 
 @Injectable()
 export class NitsService {
-  create(createNitDto: CreateNitDto) {
-    return 'This action adds a new nit';
+  private nits: Nit[] = [];
+
+  create(nit: CreateNitDto) {
+    const {
+      firstName,
+      secondName,
+      firstLastName,
+      secondLastName,
+      state,
+      description_state,
+    } = nit;
+
+    this.nits.push({
+      id: this.nits.length + 1,
+      firstName: firstName,
+      secondName: secondName,
+      firstLastName: firstLastName,
+      secondLastName: secondLastName,
+      state: state,
+      description_state: description_state,
+      updated_at: new Date(),
+      created_at: new Date(),
+    });
+
+    return 'Nit created successfully';
   }
 
   findAll() {
-    return `This action returns all nits`;
+    return this.nits;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} nit`;
+    return this.nits.find((nit) => nit.id === id);
   }
 
   update(id: number, updateNitDto: UpdateNitDto) {
+    const nit = this.nits.find((nit) => nit.id === id);
+
+    if (!nit) {
+      return 'Nit not found';
+    }
+
+    const values = Object.keys(updateNitDto);
+
+    console.log(values);
+
+    // this.nits[id].updated_at = new Date();
+    nit.updated_at = new Date();
+
     return `This action updates a #${id} nit`;
   }
 
   remove(id: number) {
+    const index = this.nits.findIndex((nit) => nit.id === id);
+
+    if (index !== -1) {
+      this.nits.splice(index, 1);
+    }
+
+    console.log(this.nits);
+
     return `This action removes a #${id} nit`;
   }
 }
