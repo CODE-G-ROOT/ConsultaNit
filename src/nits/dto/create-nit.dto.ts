fd2eb44149920eeabf1bb7257da198002cd4b6cc
JuanXhod <123758906+CODE-G-ROOT@users.noreply.github.com>
2025-01-24 21:40:18 -0500

    feat(Nits): create and update DTO for Nits
    has been added dto of create and update

--- src/nits/dto/create-nit.dto.ts
+++ src/nits/dto/create-nit.dto.ts
@@ -1,1 +1,21 @@
-export class CreateNitDto {}
+import { IsString } from 'class-validator';
+
+export class CreateNitDto {
+  @IsString()
+  public firstName: string;
+
+  @IsString()
+  public secondName: string;
+
+  @IsString()
+  public firstLastName: string;
+
+  @IsString()
+  public secondLastName: string;
+
+  @IsString()
+  public state: string;
+
+  @IsString()
+  public description_state: string;
+}
--- src/nits/dto/update-nit.dto.ts
+++ src/nits/dto/update-nit.dto.ts
@@ -1,4 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateNitDto } from './create-nit.dto';
+import { IsString } from 'class-validator';
+import { Optional } from '@nestjs/common';
 
-export class UpdateNitDto extends PartialType(CreateNitDto) {}
+export class UpdateNitDto extends PartialType(CreateNitDto) {
+  @IsString()
+  @Optional()
+  public firstName: string;
+
+  @IsString()
+  @Optional()
+  public secondName: string;
+
+  @IsString()
+  @Optional()
+  public firstLastName: string;
+
+  @IsString()
+  @Optional()
+  public secondLastName: string;
+
+  @IsString()
+  @Optional()
+  public state: string;
+
+  @IsString()
+  @Optional()
+  public description_state: string;
+}
