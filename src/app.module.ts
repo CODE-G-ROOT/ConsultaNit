74f8f9118915d6f3e71fe29c7d4bc7888d447aaa
JuanXhod <123758906+CODE-G-ROOT@users.noreply.github.com>
2025-01-24 21:37:07 -0500

    feat: :sparkles: add logger
    a rounting record has been added for the server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { RegisterModule } from './register/register.module';
 import { RazonSocialModule } from './razon_social/razon_social.module';
 import { NitsModule } from './nits/nits.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+class LoggerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
 
 @Module({
-  imports: [UsersModule, RegisterModule, RazonSocialModule, NitsModule],
+  imports: [
+    UsersModule,
+    RegisterModule,
+    RazonSocialModule,
+    NitsModule,
+    LoggerModule,
+  ],
 })
 export class AppModule {}
